6bf42cbcd2f317a9e2f4310a31dc5c80821f85ce
Rainier Mendiola <rainier.mendiola@accenture.com>
2022-01-10 17:50:20 +0800

    updated

--- ui-next/config/index.ts
+++ ui-next/config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:5000' : 'http://py-demo-api'
+export const server = dev ? 'http://localhost:5000' : 'https://py-demo-api.azurewebsites.net'
